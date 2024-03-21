046043ff0c37c745195329d0b3d69b2422695623
tamhid-mahmod <tamhidmahmod94@gmail.com>
2024-03-21 14:03:58 +0600

    hero section added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+
+import HomeHero from "@/sections/home/HomeHero";
+
+export default function RootPage() {
+  return (
+    <MaxWidthWrapper>
+      <HomeHero />
+    </MaxWidthWrapper>
+  );
+}
