4d23653be506b2b76df982b3191eade534c99371
tamhid-mahmod <tamhidmahmod94@gmail.com>
2024-03-21 14:45:03 +0600

    added perks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 
 import HomeHero from "@/sections/home/HomeHero";
+import HomePerks from "@/sections/home/HomePerks";
 
 export default function RootPage() {
   return (
-    <MaxWidthWrapper>
-      <HomeHero />
-    </MaxWidthWrapper>
+    <>
+      <MaxWidthWrapper>
+        <HomeHero />
+
+        {/* TODO: List products */}
+      </MaxWidthWrapper>
+
+      <HomePerks />
+    </>
   );
 }
